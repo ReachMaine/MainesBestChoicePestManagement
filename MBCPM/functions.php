e0d569bdf496814cc9c492042a60f40ce79a4089
linda@reachmaine.com <linda@reachmaine.com>
2018-04-04 08:19:51 -0400

    initial upload

---
+++ MBCPM/functions.php
@@ -0,0 +1,8 @@
+<?php
+
+  require_once(get_stylesheet_directory().'/custom/branding.php');
+  require_once(get_stylesheet_directory().'/custom/language.php');
+  require_once(get_stylesheet_directory().'/custom/reach_CTAs.php');
+  require_once(get_stylesheet_directory().'/custom/oshine.php');
+
+?>
